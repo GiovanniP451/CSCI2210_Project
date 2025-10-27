3c9f93f700818c3d929d9dd14081b7d28fd24dc0
Giovanni Pernudi <pernudi.giovanni@gmail.com>
2025-10-27 02:50:00 -0600

    Create Inventory.java

---
+++ Stage 3/Inventory.java
@@ -0,0 +1,33 @@
+
+import java.util.ArrayList;
+
+
+public class Inventory {
+    private ArrayList<Item> Items;
+
+    //Initializes inventory list
+    public Inventory(){
+    Items = new ArrayList<>();
+}
+    //add product to inventory
+    public void addItem(Item Items){
+        Items.add(Item);
+    }
+
+    //removes product
+    public void removeItem(Items Items){
+        Items.remove(Item);
+    }
+
+
+    //show inventory list if anything is in it
+    public void showAllItems(){
+        if (Items.isEmpty()){
+            System.out.println("No Items in inventory.");
+        }else{
+            for(Item product:Items){
+                System.out.println(Item);
+            }
+        }
+    }
+}
