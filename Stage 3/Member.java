7b0e22dfd820098fb2bf3e23d6b9de6301dc79ee
Giovanni Pernudi <pernudi.giovanni@gmail.com>
2025-10-27 02:33:59 -0600

    Create Member.java

---
+++ Stage 3/Member.java
@@ -0,0 +1,46 @@
+public class Member {
+   
+	
+    private String name;
+    private String username;
+    private String password;
+    
+    
+    public Member(String name, String username, String password) {
+        this.name = name;
+        this.username = username;
+        this.password = password;
+    }
+    
+    //explicit parameters the name and age and assigns them to the instance variables
+    public String getName() {
+        return name;
+    }
+    
+    
+    public String getUsername() {
+        return username;
+    }
+    
+    public String getPassword() {
+    	return password;
+    }
+    
+    //receives as explicit parameter the name of the member and assigns it to the instance variable.
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+   
+    public String setUsername(String username) {
+        this.username = username;
+    }
+    
+    public String setPassword(String password) {
+    	this.password = password; 
+    }
+    
+    
+    
+}
+
