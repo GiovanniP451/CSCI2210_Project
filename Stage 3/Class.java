020c4a7377d15f0690f8a45e38f1c7d077e3bbd1
Giovanni Pernudi <pernudi.giovanni@gmail.com>
2025-11-02 01:29:07 -0600

    Create Class.java

---
+++ Stage 3/Class.java
@@ -0,0 +1,34 @@
+package csci2210;
+
+//author: Giovanni Pernudi
+
+import java.util.ArrayList;
+
+public class Class {
+	
+	private String className;
+	private String startTime;
+	private String endTime;
+	private int maxCapacity;
+	private ArrayList<Member> enrolledMembers;
+	private ArrayList<Staff> staff;
+
+	
+	public boolean register() {
+		
+	}
+	
+	public void cancel(membership member) {
+		
+	}
+	
+	public int spotsLeft() {
+	}
+	
+	public boolean isFull() {
+	}
+	
+	public void getRoster() {
+	}
+	
+}
