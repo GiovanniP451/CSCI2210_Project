f07621c174addc7f4b1a7e5fa591ae00aeaf27b4
Giovanni Pernudi <pernudi.giovanni@gmail.com>
2025-11-02 01:11:30 -0600

    Create Item.java

---
+++ Stage 3/Item.java
@@ -0,0 +1,23 @@
+package csci2210;
+
+public class Item {
+	
+	private int id;
+	private String name;
+	private int quantity;
+	
+	public Item(int id, String name, int quantity) {
+		this.id = id;
+		this.name = name;
+		this.quantity = quantity;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+}
