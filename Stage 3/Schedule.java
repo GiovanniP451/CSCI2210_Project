1ebf462793e3e518568da69aff979aab7c1481ee
Giovanni Pernudi <pernudi.giovanni@gmail.com>
2025-11-04 00:52:59 -0700

    Create Schedule.java

---
+++ Stage 3/Schedule.java
@@ -0,0 +1,14 @@
+//package csci2210;
+//Author: Giovanni Pernudi 
+
+import java.util.ArrayList;
+public class Schedule {
+	private ArrayList<Class> classes;
+	private ArrayList<Staff> staffShifts;
+	
+	public Schedule() {
+		
+	}
+
+	
+}
