03d5d235eb59ac21aa483fedefa18a3646bdbab8
Giovanni Pernudi <pernudi.giovanni@gmail.com>
2025-11-14 01:33:05 -0700

    Update CashPayment.java

--- Stage 3/CashPayment.java
+++ Stage 3/CashPayment.java
@@ -2,6 +2,14 @@
 
 @Author Isiah John
 */
+package csci2210;
+
+/* 
+ * This class allows payments to be created when processing member
+ * dues, class fees, or other transactions. 
+ */
+
+//Two instance variables. Cash given and changeDue. 
 public class CashPayment extends Payment{
     private double cashGiven;
     private double changeDue;
@@ -12,16 +20,22 @@ public class CashPayment extends Payment{
         super.setMethod("Cash");
     }
     
+    //Method to get the amount of cash that the customer hands over. 
     public double getCashGiven()
     {
         return cashGiven;
     }
+    
+    //method to return what the amount of the change is due. 
     
     public double getChangeDue()
     {
         return changeDue;
     }
     
+    //method to run calculations to process the payment 
+    //if the cash given is less than the total then false is thrown
+    //If the cash given is the right amount, it will return the amount given back as change. 
     public boolean processPayment()
     {
         double total = calculateTotal();
@@ -38,6 +52,7 @@ public class CashPayment extends Payment{
     }
     
     @Override
+    //return to String method to display the information. 
     public String getPaymentDetails()
     {
         return "Cash Given: $" + String.format("%.2f",cashGiven) + "\n"
