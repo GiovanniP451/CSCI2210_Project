6f8c0475db80bf459cfb99e872467646f286d490
Giovanni Pernudi <pernudi.giovanni@gmail.com>
2025-11-16 00:54:03 -0700

    Update CreditCardPayment.java

--- Stage 3/CreditCardPayment.java
+++ Stage 3/CreditCardPayment.java
@@ -2,10 +2,16 @@
 
 @Author Isiah John
 */
+
+/*
+ * Class to actually process the payment with the credit cards.
+ * Extends payment to be able to use the same code.
+ */
 public class CreditCardPayment extends Payment{
     private CreditCard card;
     private Member member;
     
+    //Object to be store the member and the credit card //information.
     public CreditCardPayment(Member member, CreditCard card)
     {
         this.member = member;
@@ -13,6 +19,7 @@ public class CreditCardPayment extends Payment{
         super.setMethod("Credit Card"); //Uses Payment methods
     }
 
+    //list of getters for information display.
     public CreditCard getCard() {
         return card;
     }
@@ -21,6 +28,8 @@ public class CreditCardPayment extends Payment{
         return member;
     }
     
+    //Checks and return a boolean value if the payment amount
+    //is correct
     public boolean processPayment()
     {
         setAmount(calculateTotal());
@@ -28,6 +37,7 @@ public class CreditCardPayment extends Payment{
     }
     
     @Override
+    //display information of the card to the user.
     public String getPaymentDetails()
     {
         return "Charged to Card: " + card.getMaskedCard() + "\n" +
